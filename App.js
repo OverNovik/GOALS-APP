2ebfe5d65bfaee31598bec5ef64b3685454cacbd
Aliaxander Novikov <nerevarin11@gmail.com>
2022-08-09 13:16:36 +0300

    add GoalItem component

--- App.js
+++ App.js
@@ -7,6 +7,7 @@ import {
   TextInput,
   View,
 } from "react-native";
+import GoalItem from "./components/GoalItem";
 
 export default function App() {
   const [enteredGoalText, setEnteredGoalText] = useState("");
@@ -36,11 +37,7 @@ export default function App() {
           data={goals}
           alwaysBounceVertical={false}
           renderItem={(itemData) => {
-            return (
-              <View style={styles.goalItem}>
-                <Text style={styles.goalText}>{itemData.item.text}</Text>
-              </View>
-            );
+            return <GoalItem text={itemData.item.text} />;
           }}
           keyExtractor={(item, index) => {
             return item.id;
@@ -75,14 +72,5 @@ const styles = StyleSheet.create({
   },
   goalsContainer: {
     flex: 5,
-  },
-  goalItem: {
-    margin: 8,
-    padding: 8,
-    borderRadius: 6,
-    backgroundColor: "#5e0acc",
-  },
-  goalText: {
-    color: "white",
   },
 });
